92c14165557c4f443bc01787485b789dd0a9063d
Michael Eden <themichaeleden@gmail.com>
2017-06-26 22:25:58 -0700

    CLI can set up part of travis.

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+#[macro_use]
+extern crate clap;
+extern crate rustache;
+
+use std::io::Write;
+use std::env;
+use std::fs::{self, File};
+use std::path::Path;
+
+use clap::App;
+
+use rustache::{HashBuilder, Render};
+
+const GIT_REV: &'static str = include_str!("../.git/refs/heads/master");
+
+const FILES: [(&'static str, &'static str); 2] = [
+    (include_str!("../templates/travis.d/build.sh"), ".travis.d/build.sh"),
+    (include_str!("../templates/travis.yml"), ".travis.yml"),
+];
+
+fn main() {
+    // get command line args
+    let args_config = load_yaml!("args.yaml");
+    let matches = App::from_yaml(args_config).get_matches();
+
+    match matches.subcommand() {
+        ("init", Some(a)) => init(a.value_of("PATH")),
+
+        _ => {
+            writeln!(&mut std::io::stderr(),
+                     "No such command, try using --help.").ok();
+            ::std::process::exit(64);
+        },
+
+    }
+}
+
+fn init(path: Option<&str>) {
+    let path = match path {
+        Some(p) => Path::new(p).to_path_buf(),
+        None => env::current_dir().unwrap(),
+    };
+
+    // check if path exists, if not create it.
+    match fs::metadata(&path) {
+        Ok(ref m) if m.is_dir() => {},
+        Ok(_) => {
+            writeln!(&mut std::io::stderr(),
+                     "{:?} exists and it not a directory!", path).ok();
+            ::std::process::exit(64);
+        },
+        Err(_) => {
+            println!("{:?} does not exist, creating it.", path);
+            fs::create_dir_all(&path).unwrap();
+        },
+    }
+
+    // easier if we just change to this dir
+    env::set_current_dir(&path).unwrap();
+
+    // add all the files:
+    let data = HashBuilder::new()
+        .insert("source_rev", GIT_REV);
+
+    for &(text, path) in FILES.iter() {
+        println!("Writing '{}'.", path);
+        let path = Path::new(path);
+        fs::create_dir_all(path.parent().unwrap()).unwrap();
+        let mut file = File::create(path).unwrap();
+        data.render(text, &mut file).unwrap();
+    }
+}
---
+++ templates/travis.d/build.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+# HACKGPROJECT VERSION: {{source_rev}}
+set -euo pipefail
+SOURCE_DIR=$(readlink -f "${BASH_SOURCE[0]}")
+SOURCE_DIR=$(dirname "$SOURCE_DIR")
+cd "${SOURCE_DIR}/.."
+set -x
+
+if ! hash docker &>/dev/null; then
+    echo "Cannot find `docker`!" >&2
+    exit 64
+fi
+if docker ps &>/dev/null; then
+    docker=docker
+elif sudo docker ps &>/dev/null; then
+    docker='sudo docker'
+fi
+
+image_name=$(basename "$(pwd)")
+
+build_project_source() {
+    if [[ -f Dockerfile.build ]]; then
+        local build_image_name="$(basename $(pwd))-build"
+        $docker build -f Dockerfile.build --rm -t "$build_image_name" .
+        $docker run -w '/src' -v "$(pwd):/src" "$build_image_name"
+    fi
+}
+
+test_project_source() {
+    if [[ -f Dockerfile.test ]]; then
+        local test_image_name="$(basename $(pwd))-test"
+        $docker build -f Dockerfile.test --rm -t "$test_image_name" .
+        $docker run -w '/src' -v "$(pwd):/src" "$test_image_name"
+    fi
+}
+
+build_project_container() {
+    $docker build -f Dockerfile --rm -t "$image_name" .
+}
+
+publish_project_container() {
+    local git_rev=$(git rev-parse HEAD)
+    local push_image_name="${REGISTRY}/${DOCKER_ID_USER}/${image_name}"
+    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+    docker tag "$image_name" "$push_image_name":"$git_rev"
+    docker push "$push_image_name"
+    docker tag "$push_image_name":"$git_rev" "$push_image_name":latest
+    docker push "$push_image_name"
+}
+
+
+build_project_source
+test_project_source
+build_project_container
+
+if [[ ${TRAVIS_BRANCH:-} = master && ${TRAVIS_PULL_REQUEST-} = false ]]; then
+    publish_project_container
+fi
+
